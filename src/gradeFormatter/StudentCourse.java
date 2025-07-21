ce54cd3d6d643790c6683fa37d113ed76535a8a7
Mehdi Al-Heloo <1mehdialheloo@gmail.com>
2025-07-21 12:55:23 -0400

    Update StudentCourse.java

--- src/gradeFormatter/StudentCourse.java
+++ src/gradeFormatter/StudentCourse.java
@@ -1,12 +1,3 @@
-/*
---------------------------------------------------
-Project: CP317 Grade Formatter
-File:    StudentCourse.java
-Author:  Mehdi Al-heloo
-Version: 2025-07-10
---------------------------------------------------
-*/
-
 package gradeFormatter;
 
 /**
